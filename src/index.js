6653592d268348ad3e87be74d816e1f029619091
Isaiah <zaymgmt707@gmail.com>
2024-07-26 13:45:40 -0700

    ipdates index.js and db.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const baseUrl = 'http://localhost:3000/dogs'
+  const tableBody = document.getElementById('table-body')
+  const dogForm = document.getElementById('dog-form')
+  let selectedDogId = null
 
+  function fetchDogs() {
+    fetch(baseUrl)
+      .then(response => response.json())
+      .then(dogs => {
+        renderDogs(dogs)
+      })
+  }
+
+  function renderDogs(dogs) {
+    tableBody.innerHTML = ''
+    dogs.forEach(dog => {
+      const tr = document.createElement('tr')
+      tr.innerHTML = `
+        <td>${dog.name}</td>
+        <td>${dog.breed}</td>
+        <td>${dog.sex}</td>
+        <td><button data-id="${dog.id}" class="edit-button">Edit</button></td>
+      `
+      tableBody.appendChild(tr)
+    })
+  }
+
+  function populateForm(dog) {
+    dogForm.name.value = dog.name
+    dogForm.breed.value = dog.breed
+    dogForm.sex.value = dog.sex
+    selectedDogId = dog.id
+  }
+
+  function updateDog(event) {
+    event.preventDefault()
+    if (selectedDogId) {
+      const updatedDog = {
+        name: dogForm.name.value,
+        breed: dogForm.breed.value,
+        sex: dogForm.sex.value
+      }
+      fetch(`${baseUrl}/${selectedDogId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedDog),
+      })
+        .then(response => response.json())
+        .then(() => {
+          fetchDogs() // Re-fetch dogs to get the updated data
+          dogForm.reset()
+          selectedDogId = null
+        })
+    }
+  }
+
+  tableBody.addEventListener('click', event => {
+    if (event.target.classList.contains('edit-button')) {
+      const dogId = event.target.dataset.id
+      fetch(`${baseUrl}/${dogId}`)
+        .then(response => response.json())
+        .then(dog => {
+          populateForm(dog)
+        })
+    }
+  })
+
+  dogForm.addEventListener('submit', updateDog)
+
+  fetchDogs()
 })
